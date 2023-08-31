5bd710bf7e2476690602a33b88eaea74a9a5c874
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-08-31 09:33:22 -0500

    Ruta mas corta

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,32 +1,41 @@
-def dijkstra(current, nodes, distances):
-    unvisited = {node: None for node in nodes}
-    visited = {}
-    currentDistance = 0
-    unvisited[current] = currentDistance
-    while True:
-        for neighbour, distance in distances[current].items():
-            if neighbour not in unvisited:
-                continue
-            newDistance = currentDistance + distance
-            if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
-                unvisited[neighbour] = newDistance
-        visited[current] = currentDistance
-        del unvisited[current]
-        if not unvisited:
-            break
-        candidates = [(node, dist) for node, dist in unvisited.items() if dist is not None]
-        print(sorted(candidates, key=lambda x: x[1]))
-        current, currentDistance = sorted(candidates, key=lambda x: x[1])[0]
-    return visited
+# Desarrollado por Brayan Baquero y Juliana Castillo
+# Asignatura de Inteligencia Artificial 
+# Profesor : Manuel Alexander Cadena 
+# Universidad de Cundinamarca
+# Ingenieria de Sistemas
 
-nodes = ('A', 'B', 'C', 'D', 'E')
-distances = {
-    'A': {'B': 5, 'C': 2},
-    'B': {'C': 2, 'D': 3},
-    'C': {'B': 3, 'D': 7},
-    'D': {'E': 7},
-    'E': {'D': 9}
-}
-current = 'A'
+import sys
+def dijkstra_con_direccionalidad(grafo, origen):
+    visitados = set()
+    distancia = {nodo: sys.maxsize for nodo in grafo}
+    distancia[origen] = 0
+    ruta_mas_corta = {}
+    while len(visitados) != len(grafo):
+        nodo_actual = min((nodo for nodo in grafo if nodo not in visitados), key=lambda n: distancia[n])
+        visitados.add(nodo_actual)
 
-print(dijkstra(current, nodes, distances))
+        for vecino, peso in grafo[nodo_actual].items():
+            nueva_distancia = distancia[nodo_actual] + peso
+            if nueva_distancia < distancia[vecino]:
+                distancia[vecino] = nueva_distancia
+                ruta_mas_corta[vecino] = nodo_actual
+    return distancia, ruta_mas_corta
+num_nodos = int(input("Ingrese el número de nodos: "))
+grafo = {}
+for i in range(num_nodos):
+    valor_nodo = input(f"Ingrese el valor para el nodo {i}: ")
+    grafo[valor_nodo] = {}
+for nodo in grafo:
+    num_aristas = int(input(f"Ingrese el número de aristas para el nodo {nodo}: "))
+    for i in range(num_aristas):
+        vecino = input(f"Ingrese el vecino para el nodo {nodo}: ")
+        peso = int(input(f"Ingrese el peso para la arista {nodo} -> {vecino}: "))
+        grafo[nodo][vecino] = peso
+origen = input("Ingrese el nodo de origen: ")
+distancia, ruta_mas_corta = dijkstra_con_direccionalidad(grafo, origen)
+print("Distancias más cortas desde el nodo de origen:")
+for nodo, dist in distancia.items():
+    print(f"Nodo: {nodo}, Distancia: {dist}")
+print("Ruta más corta hacia cada nodo:")
+for nodo, ruta in ruta_mas_corta.items():
+    print(f"Nodo: {nodo}, Ruta: {ruta} -> {nodo}")
